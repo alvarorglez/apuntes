ed5065ab0c91c3719091da663bd7a6cfe54a1634
alvarorglez <alvarorglez@users.noreply.github.com>
2025-10-29 10:40:53 +0000

    deploy: 3c66072632fc4408acf3080bca5e0a09bb62c691

---
+++ javascripts/katex-init.js
@@ -0,0 +1,12 @@
+
+// Auto-render KaTeX for MkDocs Material
+document$.subscribe(() => {
+  renderMathInElement(document.body, {
+    delimiters: [
+      {left: "$$", right: "$$", display: true},
+      {left: "\\[", right: "\\]", display: true},
+      {left: "\\(", right: "\\)", display: false}
+    ],
+    throwOnError: false
+  });
+});
